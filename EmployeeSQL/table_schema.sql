72fead6742b788e033fd191dff4bd48d44719393
Jessie Goodell <jessiegoodell@Jessies-MacBook-Pro.local>
2020-08-17 15:21:23 -0700

    adding table schema

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,76 @@
+-- Creating table schema
+
+-- Create table departments
+DROP TABLE departments
+
+CREATE TABLE departments (
+	dept_no VARCHAR(10),
+	dept_name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+SELECT * FROM departments
+
+-- create table employees
+DROP TABLE employees
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR (10) NOT NULL,
+	birth_data DATE NOT NULL,
+	first_name VARCHAR (20) NOT NULL,
+	last_name VARCHAR (20) NOT NULL,
+	sex VARCHAR (2) NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no) 
+);
+
+SELECT * FROM employees
+
+-- create table dept_emp
+DROP TABLE dept_emp
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dep_no VARCHAR (10) NOT NULL
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp
+
+-- create table dept_managers
+DROP TABLE dept_managers
+
+CREATE TABLE dept_managers (
+	emp_no INT NOT NULL,
+	dep_no VARCHAR (10) NOT NULL
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_managers
+
+-- create table salaries
+DROP TABLE salaries
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM salaries
+
+-- create table titles
+DROP TABLE titles
+
+CREATE TABLE titles (
+	title_id VARCHAR (10) NOT NULL,
+	title VARCHAR (20) NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM titles
+
+
